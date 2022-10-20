1fb55fede2c0745e138f05880a4c66d1d921f9f0
adhxabre <abeldustin06@gmail.com>
2022-10-20 11:04:34 +0700

    day-8

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module day-7
+module day-8
 
 go 1.19
 
--- main.go
+++ main.go
@@ -6,9 +6,39 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
+
+var Data = map[string]interface{}{
+	"Title": "Personal Web",
+}
+
+type Blog struct {
+	Title     string
+	Post_date string
+	Author    string
+	Content   string
+	Duration  string
+}
+
+var Blogs = []Blog{
+	{
+		Title:     "Pasar Coding Di Indonesia Dinilai Masih Menjanjikan",
+		Post_date: "20 October 2022 22:30 WIB",
+		Author:    "Abel Dustin",
+		Content:   "Test",
+		Duration:  "2 Bulan",
+	},
+	{
+		Title:     "Pasar Ikan Di Indonesia Dinilai Masih Menjanjikan",
+		Post_date: "20 October 2022 22:30 WIB",
+		Author:    "Abel Dustin",
+		Content:   "Test",
+		Duration:  "2 Bulan",
+	},
+}
 
 func main() {
 	route := mux.NewRouter()
@@ -21,9 +51,10 @@ func main() {
 	route.HandleFunc("/", home).Methods("GET")
 	route.HandleFunc("/contact", contact).Methods("GET")
 	route.HandleFunc("/blog", blog).Methods("GET")
-	route.HandleFunc("/blog-detail/{id}", blogDetail).Methods("GET")
+	route.HandleFunc("/blog-detail/{index}", blogDetail).Methods("GET")
 	route.HandleFunc("/form-blog", formAddBlog).Methods("GET")
 	route.HandleFunc("/add-blog", addBlog).Methods("POST")
+	route.HandleFunc("/delete-blog/{index}", deleteBlog).Methods("GET")
 
 	fmt.Println("Server running on port 5000")
 	http.ListenAndServe("localhost:5000", route)
@@ -64,6 +95,8 @@ func contact(w http.ResponseWriter, r *http.Request) {
 func blog(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
+	// fmt.Println(Blogs)
+
 	var tmpl, err = template.ParseFiles("views/blog.html")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -71,8 +104,12 @@ func blog(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	respData := map[string]interface{}{
+		"Blogs": Blogs,
+	}
+
 	w.WriteHeader(http.StatusOK)
-	tmpl.Execute(w, nil)
+	tmpl.Execute(w, respData)
 }
 
 func blogDetail(w http.ResponseWriter, r *http.Request) {
@@ -85,13 +122,26 @@ func blogDetail(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+	var BlogDetail = Blog{}
+
+	index, _ := strconv.Atoi(mux.Vars(r)["index"])
+
+	for i, data := range Blogs {
+		if index == i {
+			BlogDetail = Blog{
+				Title:     data.Title,
+				Content:   data.Content,
+				Post_date: data.Post_date,
+				Author:    data.Author,
+			}
+		}
+	}
 
 	data := map[string]interface{}{
-		"Title":   "Pasar Coding di Indonesia Dinilai Masih Menjanjikan.",
-		"Content": "REPUBLIKA.CO.ID, JAKARTA -- Ketimpangan sumber daya manusia (SDM) disektor digital masih menjadi isu yang belum terpecahkan. Berdasarkan penelitian ManpowerGroup.REPUBLIKA.CO.ID, JAKARTA -- Ketimpangan sumber daya manusia (SDM) disektor digital masih menjadi isu yang belum terpecahkan. Berdasarkan penelitian ManpowerGroup.REPUBLIKA.CO.ID, JAKARTA -- Ketimpangan sumber daya manusia (SDM) disektor digital masih menjadi isu yang belum terpecahkan. Berdasarkan penelitian ManpowerGroup.",
-		"Id":      id,
+		"Blog": BlogDetail,
 	}
+
+	// fmt.Println(data)
 
 	w.WriteHeader(http.StatusOK)
 	tmpl.Execute(w, data)
@@ -120,5 +170,30 @@ func addBlog(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Title : " + r.PostForm.Get("inputTitle")) // value berdasarkan dari tag input name
 	fmt.Println("Content : " + r.PostForm.Get("inputContent"))
 
+	var title = r.PostForm.Get("inputTitle")
+	var content = r.PostForm.Get("inputContent")
+
+	var newBlog = Blog{
+		Title:     title,
+		Content:   content,
+		Author:    "Abel Dustin",
+		Post_date: time.Now().String(),
+		Duration:  "2 Bulan",
+	}
+
+	//Blogs.push(newBlog)
+	Blogs = append(Blogs, newBlog)
+	// fmt.Println(Blogs)
+
 	http.Redirect(w, r, "/blog", http.StatusMovedPermanently)
+}
+
+func deleteBlog(w http.ResponseWriter, r *http.Request) {
+	index, _ := strconv.Atoi(mux.Vars(r)["index"])
+	fmt.Println(index)
+
+	Blogs = append(Blogs[:index], Blogs[index+1:]...)
+	fmt.Println(Blogs)
+
+	http.Redirect(w, r, "/blog", http.StatusFound)
 }
